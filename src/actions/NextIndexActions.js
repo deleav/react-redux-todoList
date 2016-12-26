e44d559394e0420cdc4e0136a88ed57e85af8316
deleav <a42006310@gmail.com>
2016-12-27 03:26:01 +0800

    something went worng

---
+++ src/actions/NextIndexActions.js
@@ -0,0 +1,12 @@
+// action type
+
+const ADD_INDEX = 'ADD_INDEX';
+
+// action creator
+
+export function addIndex( index ) {
+  return {
+    ADD_INDEX,
+    index
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,19 @@
 /* main.js */
 'use strict';
 
+import thunkMiddleware from 'redux-thunk';
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import todoApp from './reducers/AppReducers';
 
 let rootElement = document.getElementById('mountNode');
-let store = createStore( todoApp, window.devToolsExtension ? window.devToolsExtension() : undefined );
+let store = createStore(
+  todoApp,
+  applyMiddleware( thunkMiddleware ),
+  window.devToolsExtension ? window.devToolsExtension() : undefined );
 
 render(
   <Provider store={store}>
